f3029f2cb59cd7b8994990e83eabacca4312ce65
Eugene Kiprop Sang <85967099+eugenesang@users.noreply.github.com>
2021-09-27 13:28:20 +0300

    Update multiplier.h

--- multiplier.h
+++ multiplier.h
@@ -1,17 +1,65 @@
 #ifndef MULTIPLIER_H
 #define MULTIPLIER_H
+#include <iostream>
 #include <vector>
-#include <cmath>
-#include "adder.h"
 using std::vector;
-vector <int> multiply(vector <int> a, vector<int> b, int len){
- vector <int> ret={0};
- for(int i=0; i<a.size(); i++){
-     
-     for(int j=0; j<a[i]*pow(len, i); j++){
-         ret=add(ret, a, len);
-     }
- }
- return ret;
+using std::cout;
+using std::endl;
+vector <int> times_vec(vector <int> &a, int x)
+{
+    for(int &i : a)
+    {
+        i=i*x;
+    }
+    return a;
+}
+int floor_div(int num, int den)
+{
+    num -= num%den;
+    return num/den;
+}
+vector <int> fix_vector(vector <int> & vec, int max)
+{
+    int carry=0;
+    for(int &i : vec)
+    {
+        i+=carry;
+        if(i>max)
+        {
+            carry=floor_div(i, max);
+            i%=max;
+        }else
+        {
+            carry=0;
+        }
+    }
+    if(carry>0)
+    {
+        vec.push_back(carry);
+        fix_vector(vec, max);
+    }
+    return vec;
+}
+vector <int> multiply(vector <int> multiple, vector <int> multiplier)
+{
+    vector < vector <int> > answer;
+    vector <int> final;
+    for(int i=0; i<multiple.size(); i++)
+    {
+        answer.push_back(times_vec(multiplier, i));
+    }
+    for(int i=0; i<answer.size(); i++)
+    {
+        for(int j=0; j<answer[i].size(); j++)
+        {
+            if(final[j+i]){
+                final[j+i]+=answer[i][j];
+            }else{
+                final[j+i]=answer[i][j];
+            }
+        }
+    }
+    
+    return fix_vector(final, 10);
 }
 #endif
