7295c99e7a7a91073bdf41fd8f7448695e032ad1
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-02-20 20:23:52 +0300

    fix readability

--- numberSystem.h
+++ numberSystem.h
@@ -1,29 +1,31 @@
 #include <vector>
 #include <iostream>
+
 using std::cout;
 using std::vector;
 
-//#define max=10;
-//lets define some helpers that will be used throughout the program
+// #define max=10;
+// lets define some helpers that will be used throughout the program
 
-// 1. this function makes sure that no member of the vector exeeds the specified maximum value,
+// 1. this function makes sure that no member of the vector exceeds the specified maximum value,
 
-vector <int> fix_vector(vector <int> & vec,int max)
+vector<int> fix_vector(vector<int> &vec, int max)
 {
-    int carry=0;
-    for(int &i : vec)
+    int carry = 0;
+    for (int &i : vec)
     {
-        i+=carry;
-        if(i>max)
+        i += carry;
+        if (i > max)
         {
-            carry=i/max;
-            i%=max;
-        }else
+            carry = i / max;
+            i %= max;
+        }
+        else
         {
-            carry=0;
+            carry = 0;
         }
     }
-    if(carry>0)
+    if (carry > 0)
     {
         vec.push_back(carry);
         fix_vector(vec, max);
@@ -33,112 +35,136 @@ vector <int> fix_vector(vector <int> & vec,int max)
 
 // 2. this function fills a vector with zeros without affecting is value
 
-vector <int> fillVec(vector <int> _short, int length)
+vector<int> fillVec(vector<int> _short, int length)
 {
-    length-=_short.size();
-    vector <int> ret;
-    while (length>0)
+    length -= _short.size();
+    vector<int> ret;
+    while (length > 0)
     {
         ret.push_back(0);
         length--;
     }
-    for (int element: ret)
+    for (int element : ret)
     {
         _short.push_back(element);
     }
     return _short;
 }
-// 3. this function heleps clean a vector by removing unnnecesary 0's
-vector <int> clean_vec(vector <int> a){
-    vector <int >ret;
-    vector <int> zero_carier;
-    for(int i : a){
-        if(i==0){
-            zero_carier.push_back(i);
-        }else{
-            for(int o : zero_carier){
+
+// 3. this function helps clean a vector by removing unnecessary 0's
+
+vector<int> clean_vec(vector<int> a)
+{
+    vector<int> ret;
+    vector<int> zero_carrier;
+    for (int i : a)
+    {
+        if (i == 0)
+        {
+            zero_carrier.push_back(i);
+        }
+        else
+        {
+            for (int o : zero_carrier)
+            {
                 ret.push_back(o);
             }
             ret.push_back(i);
-            zero_carier={};
+            zero_carrier = {};
         }
     }
     return ret;
 }
+
 // 4. this function compares two vectors a and b and returns 1 if a is greater than b,2 if b is greater and 3 if they are equal;
-int bigger_vector(vector <int> a, vector <int> b)
+
+int bigger_vector(vector<int> a, vector<int> b)
 {
-    a=clean_vec(a);
-    b=clean_vec(b);
- if(a.size()>b.size())
- {
-     return 1;
- }else if(b.size()>a.size())
- {
- return 2;
- } else
- {
-     for(int i=0; i<a.size(); i++)
-     {
-         if(a[i]>b[i]) return 1;
-         if(b[i]>a[i]) return 2;
-     }
- }
- return 3;
+    a = clean_vec(a);
+    b = clean_vec(b);
+    if (a.size() > b.size())
+    {
+        return 1;
+    }
+    else if (b.size() > a.size())
+    {
+        return 2;
+    }
+    else
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] > b[i])
+                return 1;
+            if (b[i] > a[i])
+                return 2;
+        }
+    }
+    return 3;
 }
 
-//adition 
+// addition
 
-vector <int> add(vector <int> a, vector <int> b, int maxlength)
+vector<int> add(vector<int> a, vector<int> b, int maxlength)
 {
-    int carry=0;
-    vector <int> vf;
-    if(a.size()>b.size()){ b=fillVec(b, a.size());}
-    if(b.size()>a.size()){ a=fillVec(a, b.size());}
-    for(int i=0; i<a.size(); i++)
+    int carry = 0;
+    vector<int> vf;
+    if (a.size() > b.size())
     {
-        int n=a[i]+b[i]+carry;
+        b = fillVec(b, a.size());
+    }
+    if (b.size() > a.size())
+    {
+        a = fillVec(a, b.size());
+    }
+    for (int i = 0; i < a.size(); i++)
+    {
+        int n = a[i] + b[i] + carry;
         vf.push_back(n);
     }
     fix_vector(vf, maxlength);
     return vf;
 }
 
-//subtraction
+// subtraction
 
-vector <int> subtract(vector <int> x, vector <int> y, int max)
+vector<int> subtract(vector<int> x, vector<int> y, int max)
 {
-    vector <int> a, b;
-    int sign=1;
-    if(bigger_vector(x, y)==1)
+    vector<int> a, b;
+    int sign = 1;
+    if (bigger_vector(x, y) == 1)
     {
-        a=x; 
-        b=y;
-    }else{
-        a=y;
-        b=x;
-        sign=-1;
+        a = x;
+        b = y;
     }
-    if(a.size()>b.size())
+    else
     {
-        b=fillVec(b, a.size());
-    }else
-    {
-        a=fillVec(a, b.size());
+        a = y;
+        b = x;
+        sign = -1;
     }
-    int borrow=0;
-    vector <int> ret;
-    for (int i=0; i<a.size(); i++)
+    if (a.size() > b.size())
+    {
+        b = fillVec(b, a.size());
+    }
+    else
+    {
+        a = fillVec(a, b.size());
+    }
+    int borrow = 0;
+    vector<int> ret;
+    for (int i = 0; i < a.size(); i++)
     {
         int dif;
-        if(a[i]<(b[i]+borrow))
+        if (a[i] < (b[i] + borrow))
         {
-            borrow=1;
-            dif=max+a[i]-b[i];
-        }else
+            borrow = 1;
+            dif = max + a[i] - b[i];
+        }
+        else
         {
-            dif=a[i]-b[i]-borrow;
-            borrow=0;
+            dif = a[i] - b[i] - borrow;
+            borrow = 0;
         }
         ret.push_back(dif);
     }
@@ -146,44 +172,44 @@ vector <int> subtract(vector <int> x, vector <int> y, int max)
     return ret;
 }
 
-//multiplication part
+// multiplication part
 
 // we need a helper function to make it faster
-//this function will multiply a vector with an integer
+// this function will multiply a vector with an integer
 
-vector <int> times_vec(vector <int> a, int x)
+vector<int> times_vec(vector<int> a, int x)
 {
-    vector <int> b;
-    for(int i : a)
+    vector<int> b;
+    for (int i : a)
     {
-        b.push_back(i*x);
+        b.push_back(i * x);
     }
     return b;
 }
 
-
-vector <int> multiply(vector <int> multiple, vector <int> multiplier)
+vector<int> multiply(vector<int> multiple, vector<int> multiplier)
 {
-    vector < vector <int> > answer;
-    
-    for(int i=0; i<multiple.size(); i++)
+    vector<vector<int>> answer;
+
+    for (int i = 0; i < multiple.size(); i++)
     {
-        vector <int> cur=times_vec(multiplier, multiple[i]);
+        vector<int> cur = times_vec(multiplier, multiple[i]);
         answer.push_back(cur);
     }
-    vector <int> final; int iter=0;
-    while (iter < answer.size()+answer[0].size()+2)
+    vector<int> final;
+    int iter = 0;
+    while (iter < answer.size() + answer[0].size() + 2)
     {
         final.push_back(0);
         iter++;
     }
-    int i_c=0;
-    for(vector <int> i : answer)
+    int i_c = 0;
+    for (vector<int> i : answer)
     {
-         int j_c=0;
-        for(int j : i)
+        int j_c = 0;
+        for (int j : i)
         {
-            final[i_c+j_c]+=j;
+            final[i_c + j_c] += j;
             j_c++;
         }
         i_c++;
@@ -191,16 +217,18 @@ vector <int> multiply(vector <int> multiple, vector <int> multiplier)
     return fix_vector(final, 10);
 }
 
-//division --the most complicated to impliment--
-//first of all we need to find a list of all multiples of the divisor between zero and the maximum number in the system
+// division --the most complicated to implement --
+// first of all we need to find a list of all multiples of the divisor between zero and the maximum number in the system
 
-vector < vector <int> > generate_mulitiples(vector <int> divisor, int max){
-vector < vector <int> > ret;
-for(int i=0; i<max; i++)ret.push_back(times_vec(divisor, i));
-return ret;
+vector<vector<int>> generate_multiples(vector<int> divisor, int max)
+{
+    vector<vector<int>> ret;
+    for (int i = 0; i < max; i++)
+        ret.push_back(times_vec(divisor, i));
+    return ret;
 }
 
-/* 
+/*
 we need to keep track of the following
 1. the current value
 2. the closest match of the current value in the list of multiples of the divisor
@@ -208,38 +236,44 @@ we need to keep track of the following
 4. the number of decimal points
 5. the reminder
 
-in every iteration we need to check 
-1. if the value of remainder has occured twice --recuring
+in every iteration we need to check
+1. if the value of remainder has occurred twice --recurring
 2. number of decimal places has exceeded the reasonable size
 3. if the value of the remainder is zero
 
 */
 
 // the initial number;
-vector <int> first_number(vector <int> a, vector <vector <int>> multiples){
-for( int i=0; i<multiples.size(); i++){
-    int val=bigger_vector(a, multiples[i]);
- //   cout<<val<<"\n";
-    if(val>=2 ){
-        return multiples[i];
+vector<int> first_number(vector<int> a, vector<vector<int>> multiples)
+{
+    for (int i = 0; i < multiples.size(); i++)
+    {
+        int val = bigger_vector(a, multiples[i]);
+        //   cout<<val<<"\n";
+        if (val >= 2)
+        {
+            return multiples[i];
+        }
     }
-}
-return multiples[multiples.size()-1];
+    return multiples[multiples.size() - 1];
 }
 
-
-vector <int> divide(vector <int> a, vector <int> b){
-vector <int> ret;
-vector < vector <int> > multiples=generate_mulitiples(b, 10);
-for(vector <int> t: multiples){
-    for(int i : t){
-        cout<<i;
+vector<int> divide(vector<int> a, vector<int> b)
+{
+    vector<int> ret;
+    vector<vector<int>> multiples = generate_multiples(b, 10);
+    for (vector<int> t : multiples)
+    {
+        for (int i : t)
+        {
+            cout << i;
+        }
+        cout << "  " << bigger_vector(a, t) << "   ";
+        for (int i : a)
+        {
+            cout << i;
+        }
+        cout << "\n";
     }
-    cout<<"  "<<bigger_vector(a, t)<<"   ";
-    for(int i: a){
-        cout<<i;
-    }
-    cout<<"\n";
-}
-return first_number(a, multiples);
+    return first_number(a, multiples);
 }
